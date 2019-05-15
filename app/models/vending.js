a75bcfbbbbc06708afe878627f7b287c5d550317
ben <bholsinger@gmail.com>
2019-05-15 15:03:02 -0600

    created data class for vending model

--- app/models/vending.js
+++ app/models/vending.js
@@ -24,3 +24,24 @@ these are the private methods used by service
 here we all use our getter methods to create any templates needed
 getter will use return and backtick to return html needed in template
 */
+
+export default class vendingModel {
+  constructor(coinsData) {
+    //add properties of model here 
+    //model has property of data requested on coin click
+    //model has string property "you purchased coke"
+    //model has string property "more coins please"
+    this.addCoins = function (coinsData) {
+      for (coinsData = 0; coinsData < 25; coinsData++) {
+        console.log('you inserted coin data')
+      }
+
+    }
+  }
+
+  get coinAlert() {
+
+    return alert('need more coins')
+  }
+  //add methods for getter here
+}
